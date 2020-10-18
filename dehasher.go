4b724bb0f31c1667f3c4d0db814896aba7437658
RSOULIAC <raphael.souliac@ynov.com>
2020-10-18 14:05:49 +0200

    V 0.2

--- dehasher.go
+++ dehasher.go
@@ -1,7 +1,8 @@
 package main
 
 /* Petit script en go permettant de faire des attaques par dictionnaire en md5
-pas tre puissant mais fait le taff*/
+pas tres puissant mais fait le taff
+ajout de regles a venir*/
 
 import (
 	"bufio"
@@ -36,6 +37,7 @@ func attack(s string, l []string) {
 			find = true
 			fmt.Printf("Password found : ")
 			fmt.Printf("%s", l[i])
+			//fmt.Printf("\nIn position : %v", i)
 			fmt.Printf("\nFrom hash : %s\n", s)
 			fmt.Printf("Found in : ")
 			break
@@ -48,15 +50,48 @@ func attack(s string, l []string) {
 	}
 }
 
+/*func otozero(s []rune, r int) []string {
+	list := make([]string, r)
+	for i := r; i > 0; i-- {
+
+	}
+}
+
+func attackwithrules(s string, l []string, r string) {
+	lenght := len(l)
+	find := false
+	var rule []string
+	count := 0
+	for i := 0; i < lenght; i++ {
+		tab := []rune(l[i])
+		data := hashfromstring(l[i])
+		if data == s {
+			find = true
+			break
+		} else {
+			for j := 0; j < len(tab)-1; j++ {
+				if tab[j] == 'o' || tab[j] == 'O' {
+					count++
+				}
+			}
+			if count > 0 {
+				count *= count
+			}
+		}
+	}
+}*/
+
 func main() {
 	logo()
 	hash := flag.String("h", "", "--hash ; the hash you want to crack( only md5)")
 	list := flag.String("l", "", "--list ; the wordlist you want to use (.txt)")
+	//rules := flag.String("r", "", "--rules ; rules you want to add for bruteforce ")
 	flag.Parse()
-	hashstr := *hash
+	hashpnt := *hash
 	liststr := *list
+	//rulespnt := *rules
 	file, err := os.Open(liststr)
-	if len(hashstr) == 32 && err == nil {
+	if len(hashpnt) == 32 && err == nil {
 		timeinit := time.Now()
 		scan := bufio.NewScanner(file)
 		scan.Split(bufio.ScanLines)
@@ -65,7 +100,12 @@ func main() {
 			txtlines = append(txtlines, scan.Text())
 		}
 		fmt.Printf("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n")
-		attack(hashstr, txtlines)
+		/*if rulespnt != "" {
+			attackwithrules(hashpnt, txtlines, rulespnt)
+		} else {
+			attack(hashpnt, txtlines)
+		}*/
+		attack(hashpnt, txtlines)
 		timefin := time.Now()
 		fmt.Printf("%v", timefin.Sub(timeinit))
 		fmt.Printf("\n+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n")
